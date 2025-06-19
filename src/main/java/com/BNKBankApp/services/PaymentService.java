0c4c1d88db593b1a5da509a1dcc2c02635272ab4
Aboje Edwin <abojeedwin@gmail.com>
2025-06-19 23:37:58 +0100

    Building the payment service

--- src/main/java/com/BNKBankApp/services/PaymentService.java
+++ src/main/java/com/BNKBankApp/services/PaymentService.java
@@ -1,12 +1,64 @@
 package com.BNKBankApp.services;
+import com.BNKBankApp.data.model.PaymentDetails;
+import com.BNKBankApp.data.model.PaymentRequest;
+import com.BNKBankApp.data.model.PaymentResponse;
 import com.BNKBankApp.simulator.BankSimulator;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class PaymentService {
 
-    private final BankSimulator bankSimulator;
+    private BankSimulator bankSimulator;
     private  PaymentTransactionService paymentTransactionService;
 
 
+    public PaymentService(BankSimulator bankSimulator, PaymentTransactionService paymentTransactionService) {
+        this.bankSimulator = bankSimulator;
+        this.paymentTransactionService = paymentTransactionService;
+    }
+
+    public String generateTransactionId() {
+        return UUID.randomUUID().toString();
+    }
+
+    public boolean validatePaymentRequest(PaymentRequest paymentRequest) {
+        if(paymentRequest == null){
+            return false;
+        }
+        return paymentRequest.getCardNumber() != null &&
+                paymentRequest.getCardNumber().length() == 16 &&
+                paymentRequest.getCurrency()  != null &&
+                paymentRequest.getCvv() != null &&
+                paymentRequest.getExpiryDate() != null;
+    }
+
+    public PaymentResponse processPayment (PaymentRequest paymentRequest){
+        PaymentResponse paymentResponse = new PaymentResponse();
+        String transactionId = generateTransactionId();
+
+        if(!validatePaymentRequest(paymentRequest)){
+            paymentResponse.setStatus("Failed");
+            paymentResponse.setMessage("Invalid Payment Request");
+            return paymentResponse;
+        }
+        double totalAmount = paymentRequest.getAmount() * BankSimulator.TRANSACTION_FEE;
+
+        if(paymentTransactionService.isTransactionDuplicate(transactionId, paymentRequest.getCardNumber(), paymentRequest.getAmount())){
+            paymentResponse.setStatus("Duplicate transaction with : " + transactionId);
+            paymentResponse.setMessage("Duplicate transaction");
+            return paymentResponse;
+        }
+
+        paymentResponse = bankSimulator.retrieveFromBank(paymentRequest.getCardNumber(), paymentRequest.getAmount());
+        paymentResponse.setTransactionId(transactionId);
+        paymentResponse.setTotalAmount(totalAmount);
+        paymentResponse.setCurrency(paymentRequest.getCurrency());
+        paymentResponse.setOriginalAmount(paymentRequest.getAmount());
+
+        PaymentDetails paymentDetails = new PaymentDetails(transactionId,paymentRequest.getCardNumber(),paymentRequest.getAmount(),totalAmount);
+        paymentTransactionService.storePaymentDetails(paymentDetails);
+        return paymentResponse;
+    }
 }
