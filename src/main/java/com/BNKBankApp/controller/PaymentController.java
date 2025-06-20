495e2f9cc8223c589f2469b72af97a4fa126ae0f
Aboje Edwin <abojeedwin@gmail.com>
2025-06-20 01:15:27 +0100

    Building the controllers

---
+++ src/main/java/com/BNKBankApp/controller/PaymentController.java
@@ -0,0 +1,38 @@
+package com.BNKBankApp.controller;
+import com.BNKBankApp.data.model.PaymentDetails;
+import com.BNKBankApp.data.model.PaymentRequest;
+import com.BNKBankApp.data.model.PaymentResponse;
+import com.BNKBankApp.exception.PaymentDetailsNotFoundException;
+import com.BNKBankApp.services.PaymentService;
+import com.BNKBankApp.services.PaymentTransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@SpringBootApplication
+@RestController
+public class PaymentController {
+
+    @Autowired
+    PaymentService paymentService;
+    @Autowired
+    PaymentTransactionService paymentTransactionService;
+
+    @PostMapping("/processPayment")
+    public ResponseEntity<PaymentResponse> processPayment(@RequestBody PaymentRequest paymentRequest){
+        return  ResponseEntity.ok(paymentService.processPayment(paymentRequest));
+    }
+
+    @GetMapping("/{transactionId}")
+    @ExceptionHandler(PaymentDetailsNotFoundException.class)
+    public ResponseEntity <PaymentDetails> getPaymentDetails(@PathVariable String transactionId) throws Exception {
+        PaymentDetails paymentDetails = paymentTransactionService.getPaymentDetails(transactionId);
+            if(paymentDetails != null ){
+                return ResponseEntity.ok(paymentDetails);
+            }else throw new PaymentDetailsNotFoundException("Payment Details Not Found");
+        }
+
+    }
+
